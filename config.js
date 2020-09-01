00e3b8c51e4fee66b21b7839aaf02fa476ce2c30
Moha Bedr <you@example.com>
2020-09-01 19:11:14 +0100

    fixed config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: `mongodb+srv://SlayMedBedr:nHsuKXTGhNZOxojR@slayer.x9voh.mongodb.net/merng?retryWrites=true&w=majority`,
+    SECRET__KEY: "some very secret key",
+};
