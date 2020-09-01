8fc05eef8240ebf311759cfb8e53516ba4869266
Moha Bedr <you@example.com>
2020-09-01 19:24:49 +0100

    dropped config  js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: `mongodb+srv://SlayMedBedr:nHsuKXTGhNZOxojR@slayer.x9voh.mongodb.net/merng?retryWrites=true&w=majority`,
-    SECRET__KEY: "some very secret key",
-};
