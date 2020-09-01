464b686996749d327c95899b28aec24558fe21dd
Moha Bedr <you@example.com>
2020-09-01 18:55:11 +0100

    fix of name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDefs = require("./GraphQl/typeDefs");
 const resolvers = require("./GraphQl/resolvers");
 const { MONGODB } = require("./config.js");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
     typeDefs,
